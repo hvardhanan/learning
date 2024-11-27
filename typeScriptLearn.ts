8d5fe5701d9f33e1c3367203938ce089cafb5643
Harsha Vardhanan <mail.harshavardhanan@gmail.com>
2024-11-27 21:28:50 +0530

    TypeScript annotations added to typeScriptLearn

---
+++ typeScriptLearn.ts
@@ -0,0 +1,27 @@
+/////// Annotations \\\\\\\
+
+// let myVar : type = value
+
+let myName : String = "Harsha";
+myName = "Harsha Vardhanan";
+console.log(myName) // Harsha Vardhanan
+
+// myName = 12; ERROR
+
+let myAge : Number = 13;
+myAge = 18;
+console.log(myAge) // 18
+
+// myAge = "13"; ERROR
+
+let isStudent : Boolean = true;
+isStudent = false;
+console.log(isStudent) // false
+
+// isStudent = 1; ERROR
+
+
+
+
+
+
