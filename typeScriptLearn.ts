f1d6308ed65e5eb950098c52b7522f168aef9148
Harsha Vardhanan <mail.harshavardhanan@gmail.com>
2024-11-27 21:46:41 +0530

    add Type Inference to typeScriptLearn

--- typeScriptLearn.ts
+++ typeScriptLearn.ts
@@ -21,7 +21,17 @@ console.log(isStudent) // false
 // isStudent = 1; ERROR
 
 
+/////// Type Inference \\\\\\\
+// Tries to automatically determine the type of variable based on it's values
+let tech = "TypeScript";
+let version = 3.7;
+let isLatest = true;
 
+console.log(tech, version, isLatest) // TypeScript 3.7 true
+console.log(typeof tech, typeof version, typeof isLatest) // string number boolean
 
-
+// Error
+// tech = 3.7;
+// version = "TypeScript";
+// isLatest = "Nope";
 
